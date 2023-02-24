10ccc7b28edc28bd58932ba8d2f032cf916cdab1
gmaaddi <grm76@drexel.edu>
2023-02-24 14:32:03 -0500

    :bento: Create responsive header

--- wp-content/themes/idm250-theme/header.php
+++ wp-content/themes/idm250-theme/header.php
@@ -1,9 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IDM250</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- <link rel="stylesheet" href="/wp-content/themes/idm250-theme/dist/styles/main.css"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+  <?php wp_head();?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+<div class="menu-container">
+  <input type="checkbox" id="nav-check">
+  <div class="nav-header">
+    <div class="nav-title">
+      <img src="<?php echo get_template_directory_uri(); ?>/dist/graphics/logo-group.png" class="logo" alt="Logo">
+    </div>
+  </div>
+
+  <div class="nav-btn">
+    <label class="nav-label" for="nav-check">
+      <span class="nav-check"></span>
+      <span class="nav-check"></span>
+      <span class="nav-check"></span>
+    </label>
+  </div>
+  
+  <div class="menu-links">
+    <?php
+      wp_nav_menu(
+        [
+          'theme-location' => 'primary-menu',
+          'container_class' => "menu-nav",
+          'menu_class' => "menu-list",
+		      'container' => "nav",
+        ]
+      );
+    ?>
+  </div>
+</div>
+</header>
