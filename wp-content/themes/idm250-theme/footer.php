7b0c04b30bc7676fd0e932de5aa7334f65bd2b30
gmaaddi <grm76@drexel.edu>
2023-02-24 15:51:23 -0500

    :bento: Add footer content

--- wp-content/themes/idm250-theme/footer.php
+++ wp-content/themes/idm250-theme/footer.php
@@ -1,3 +1,23 @@
 <?php wp_footer();?>
+
+
+<footer>
+<img src="<?php echo get_template_directory_uri(); ?>/dist/graphics/logo-group-bigger.png" class=" logo-footer" alt="Logo">
+
+<?php
+      wp_nav_menu(
+        [
+          'theme-location' => 'footer-menu',
+          'container_class' => "menu-nav__footer",
+          'menu_class' => "menu-list__footer",
+          'container' => "nav",
+        ]
+      );
+    ?>
+
+<p class="footer-text">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+</div>
+</footer>
+
 </body>
 </html>
