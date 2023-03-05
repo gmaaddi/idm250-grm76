14f03ad42abf525685b775449731f8aac9ac60ad
gmaaddi <grm76@drexel.edu>
2023-03-05 12:42:22 -0500

    :bento: Add single.php content

--- wp-content/themes/idm250-theme/single.php
+++ wp-content/themes/idm250-theme/single.php
@@ -1,9 +1,24 @@
 <?php get_header() ?>
 <article id="post-<?php the_ID();?>" <?php post_class(); ?>>
-    <?php get_template_part('components/content'); ?>
 </article>
 
-<h2>Categories for this post</h2>
+<div class="single-wrapper">
+    <div class="single-header">
+        <h5 class="single-header__date"><?php echo get_the_date() ?></h5>
+        <h1 class="single-header__title"><?php echo get_the_title();?></h1>
+    </div>
+
+    <div class="single-header__img"
+        style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')">
+    </div>
+
+    <?php get_template_part('components/content'); ?>
+</div>
+
+
+
+
+<!-- <h2>Categories for this post</h2>
 <?php
 $currentPostId = get_the_id();
 $terms = get_the_terms($currentPostId, 'category');
@@ -11,6 +26,6 @@ $terms = get_the_terms($currentPostId, 'category');
 foreach ($terms as $term) {
     echo "<li>{$term->name}</li>";
 }
-?>
+?> -->
 
 <?php get_footer() ?>
