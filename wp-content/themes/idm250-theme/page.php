198caccdd41c92d519c0b46ee80ebe6b195765cd
gmaaddi <grm76@drexel.edu>
2023-03-11 14:55:33 -0500

    :bento: Update with comments page.php

--- wp-content/themes/idm250-theme/page.php
+++ wp-content/themes/idm250-theme/page.php
@@ -10,8 +10,8 @@
 
 
 <div class="page-wrapper">
-    <h1><?php echo get_the_title();?></h1>
-    <div><?php echo get_the_excerpt();?></div>
+    <!-- <h1><?php echo get_the_title();?></h1>
+    <div><?php echo get_the_excerpt();?></div> -->
     <?php get_template_part('components/content'); ?>
 
     <div class="cta-container">
