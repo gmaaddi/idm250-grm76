c971208484372ab2638b9cd1df1c3c150d110315
gmaaddi <grm76@drexel.edu>
2023-03-11 12:23:21 -0500

    :fire: Remove specific pages

--- wp-content/themes/idm250-theme/page-about.php
+++
@@ -1,7 +0,0 @@
-<?php get_header() ?>
-
-<h1><?php echo get_the_title();?></h1>
-<!-- <div><?php echo get_the_excerpt();?></div> -->
-<?php get_template_part('components/content'); ?>
-
-<?php get_footer() ?>
