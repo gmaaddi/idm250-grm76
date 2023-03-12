32b0f41858a195873585cd4da2c8b766554b7b97
gmaaddi <grm76@drexel.edu>
2023-03-12 11:49:39 -0400

    :bento: Add favicon

--- wp-content/themes/idm250-theme/header.php
+++ wp-content/themes/idm250-theme/header.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <!-- <link rel="stylesheet" href="/wp-content/themes/idm250-theme/dist/styles/main.css"> -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/graphics/favicon.ico" type="image/x-icon">
   <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
   <?php wp_head();?>
 </head>
