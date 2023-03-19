e2a55d46e4a66de1760c97d0c582642793b5d8e5
gmaaddi <grm76@drexel.edu>
2023-03-19 14:22:59 -0400

    :bento: Add remaining content for about page

--- wp-content/themes/idm250-theme/templates/sidebar.php
+++ wp-content/themes/idm250-theme/templates/sidebar.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Sidebar
+Template Name: Columns
 */
 ?>
 
@@ -31,10 +31,36 @@ Template Name: Sidebar
                 <p><?php the_field('right_column_subtitle_description_right'); ?></p>
             </div>
         </div>
+    </div>
+</div>
 
+
+<div class="color-block-container" style="background-color:<?php the_field('color_block_background_color'); ?>">
+    <div class="color-block-column1">
+        <h2 class="color_block_title"><?php the_field('color_block_title'); ?></h2>
+        <h5 class="color_block_description"><?php the_field('color_block_description'); ?></h5>
+        <?php 
+                $link = get_field('color_block_link');
+                if( $link ): 
+                    $link_url = $link['url'];
+                    $link_title = $link['title'];
+                    $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <a class="cta-btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+        <?php endif; ?>
     </div>
 
+    <div class="color-block-column2">
+        <?php 
+        $image = get_field('color_block_image');
+        if( !empty( $image ) ): ?>
+            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+        <?php endif; ?>
+    </div>
+</div>
 
+<div class="page-wrapper sidebar-wrapper">
+    <?php get_template_part('components/content'); ?>
 </div>
 
 
