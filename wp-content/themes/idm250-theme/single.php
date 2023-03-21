2a654214ca5110b6abe3cb24ba29c67265250499
gmaaddi <grm76@drexel.edu>
2023-03-21 14:54:48 -0400

    :art: Clean up commented code

--- wp-content/themes/idm250-theme/single.php
+++ wp-content/themes/idm250-theme/single.php
@@ -15,17 +15,4 @@
     <?php get_template_part('components/content'); ?>
 </div>
 
-
-
-
-<!-- <h2>Categories for this post</h2>
-<?php
-$currentPostId = get_the_id();
-$terms = get_the_terms($currentPostId, 'category');
-
-foreach ($terms as $term) {
-    echo "<li>{$term->name}</li>";
-}
-?> -->
-
 <?php get_footer() ?>
